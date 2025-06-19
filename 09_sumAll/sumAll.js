b3de118e4fcecc2862976fe7497347238f9b4f97
Erwin Hitgano <163173846+Hitgan0@users.noreply.github.com>
2025-06-19 14:27:52 -0400

    Finished ex 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (typeof(n1) != 'number' || typeof(n2) != 'number') {
+        return "ERROR";
+    }
+   
+    if (n1 < 0 || n2 < 0) {
+        return "ERROR";
+    }
+    if (n1 % 1 != 0 || n2 % 1 != 0) {
+        return "ERROR";
+    }
+    
+    
+    if (n1 > n2) {
+        let temp = n2;
+        n2 = n1;
+        n1 = temp;
+    }
+    sum = 0;
+    for (let i = n1; i < (n2 + 1); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
