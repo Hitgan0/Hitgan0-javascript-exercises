27f2a5b8480dce6839e2fb00f11d06b21c9b0366
Erwin Hitgano <163173846+Hitgan0@users.noreply.github.com>
2025-06-19 13:13:57 -0400

    Completed ex 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for (let i = 0; i < toRemove.length; i++) {
+        rem = toRemove[i];
+        console.log(rem, typeof(rem))
+        for (let j = 0; j < arr.length; j++) {
+
+            if ((arr[j] == rem) && (typeof(arr[j]) == typeof(rem))) {
+                arr.splice(j, 1);
+                --j;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
