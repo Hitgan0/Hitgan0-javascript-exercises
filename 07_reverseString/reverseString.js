6a00b17ba3954d3dcc735b9a41c75df2b24429c9
Erwin Hitgano <163173846+Hitgan0@users.noreply.github.com>
2025-06-19 12:49:58 -0400

    Completed ex 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    ret = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        ret = ret + str.charAt(i);
+    }
+    return ret;
 };
 
 // Do not edit below this line
